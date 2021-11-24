4696482180f53e2a7ec3c75689375465510009dc
uddivert <uddhavswami@gmail.com>
2021-11-23 22:25:58 -0500

    added short and long optarg support

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import getopt
 import requests
@@ -6,3 +7,31 @@ def getKey():
     with open('key.txt', 'r') as file:
         data = file.read().rstrip()
         return data
+
+def printArgs():
+    print('ARGV      :', sys.argv[1:])
+    print('OPTIONS   :', options)
+    print('MODE      :', mode)
+    print('SATELLITE :', satellite)
+    print('REMAINING :', remainder)
+
+#### MAIN METHOD ####
+version = '1.0'
+satellite = ''
+satFlag = False
+modeFlag = False
+
+options, remainder = getopt.getopt(sys.argv[1:], 's:m:', ['satellite=', 
+                                                         'mode',
+                                                         ])
+for opt, arg in options:
+    if opt in ('-s', '--satellite'):
+        satellite = arg
+        satFlag = True
+
+    elif opt in ('-m', '--mode'):
+        mode = arg
+        modeFlag = True
+
+printArgs()
+## what tf does this shit mean if __name__ == "__main__":
