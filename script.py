0ee02ffa036284be1078c6ad24ed9bdc927dfc5b
uddivert <uddhavswami@gmail.com>
2021-11-24 20:42:46 -0500

    added more options

--- script.py
+++ script.py
@@ -2,7 +2,9 @@
 import sys
 import getopt
 import requests
+import argparse
 
+"""
 def getKey():
     with open('key.txt', 'r') as file:
         data = file.read().rstrip()
@@ -33,5 +35,40 @@ for opt, arg in options:
         mode = arg
         modeFlag = True
 
-printArgs()
-## what tf does this shit mean if __name__ == "__main__":
+#printArgs()
+"""
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-s", "--satellite", help="Retrieve a full or filtered list" +
+                   "of satellites in SatNOGS DB",
+                    action="store_true")
+group.add_argument("-m", "--mode", help="Radio Frequency modulation modes" +
+                   "(RF Modes) currently tracked in the SatNOGS DB database",
+                    action="store_true")
+group.add_argument("-t", "--telemetry", help="View into the Telemetry " +
+                   "objects in the SatNOGS DB database. Currently, " +
+                   "this table is inclusive of all data collected from "
+                   "satellite downlink observations",
+                   action="store_true")
+group.add_argument("-l", "--tle", help="Read-only view into the most" +
+                   "recent two-line elements (TLE) in the SatNOGS DB database",
+                   action="store_true")
+group.add_argument("-r", "--transmitter", help="View into the Transmitter" +
+                   "entities in the SatNOGS DB database. Transmitters are " +
+                   "inclusive of Transceivers and Transponders",
+                   action="store_true")
+parser.add_argument("-fs", '--format_string', type=str, help='foo help')
+
+args = parser.parse_args()
+
+if args.format_string:
+    print(args.format_string)
+if args.mode:
+    print("mode turned on")
+elif args.satellite:
+    print("satllite is turned on")
+elif args.telemetry:
+    print("telemetry is turned on")
+elif args.tle:
+    print("tle is turned on")
+
